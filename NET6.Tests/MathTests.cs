d105fd37e3f7be40d18a2893106d89cea7e18a25
shahrukhkhan532 <53635685+shahrukhkhan532@users.noreply.github.com>
2022-06-03 22:02:17 +0500

    Added Math Library

---
+++ NET6.Tests/MathTests.cs
@@ -0,0 +1,23 @@
+﻿namespace NET6.Tests;
+public class MathTests
+{
+    [Fact]
+    public void GetOddNumbers_LimitIsGreaterThanZero_ShouldReturnOddNumbersUptoLimit()
+    {
+        var math = new MathLibrary();
+
+        var result = math.GetOddNumbers(5);
+
+        // More Generic
+        Assert.NotEmpty(result);
+
+        Assert.Equal(3, result.Count());
+        
+        // More Specific
+        Assert.Contains(1, result);
+        Assert.Contains(3, result);
+        Assert.Contains(5, result);
+
+        Assert.Equal(new[] { 1, 3, 5 }, result);
+    }
+}
---
+++ NET6/Fundamentals/MathLibrary.cs
@@ -0,0 +1,12 @@
+﻿namespace NET6.Fundamentals;
+public class MathLibrary
+{
+    public IEnumerable<int> GetOddNumbers(int Limit)
+    {
+        for (int i = 0; i <= Limit; i++)
+        {
+            if(i % 2 != 0)
+                yield return i;
+        }
+    }
+}
