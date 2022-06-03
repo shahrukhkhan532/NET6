0c7c62e6ea4b7c1187e5bf8df593ed9359b12833
shahrukhkhan532 <53635685+shahrukhkhan532@users.noreply.github.com>
2022-06-03 22:48:10 +0500

    Testing void return types functions.

---
+++ NET6.Tests/Fundamentals/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace NET6.Tests.Fundamentals;
+public class ErrorLoggerTests
+{
+    [Fact]
+    public void Log_WhenCalled_ShouldSetLastErrorProperty()
+    {
+        var logger = new ErrorLogger();
+        logger.LogError("a");
+
+        Assert.Equal("a", logger.LastError);
+    }
+    [Fact]
+    public void Log_WhenCalled_ShouldRaiseException()
+    {
+        var logger = new ErrorLogger();
+
+        Assert.Throws<ArgumentNullException>(() => logger.LogError(""));
+    }
+}
---
+++ NET6/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace NET6.Fundamentals;
+public class ErrorLogger
+{
+    public string? LastError { get; set; }
+    public void LogError(string error)
+    {
+        if (string.IsNullOrEmpty(error))
+            throw new ArgumentNullException();
+        LastError = error;
+        // Write to some storage
+    }
+}
