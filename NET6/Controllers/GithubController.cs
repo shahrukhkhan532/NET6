9d04bc2e5f5d854b1a7bf94715b0b08fa758fd99
shahrukhkhan532 <53635685+shahrukhkhan532@users.noreply.github.com>
2022-06-05 03:16:18 +0500

    Create GithubController.cs

---
+++ NET6/Controllers/GithubController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NET6.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Received(Object dynamic)
+        {
+            System.Diagnostics.Debug.WriteLine(dynamic);
+            return Ok();
+        }
+    }
+}
