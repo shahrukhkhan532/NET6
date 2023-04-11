ce43c7fd29dd27601619138a7ac8063258c893bb
shahrukhkhan532 <khanxhahrukh@gmail.com>
2023-04-12 02:02:44 +0500

    sw

---
+++ powershell/start-stop-app-pool.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$remote__Machine,
+    [string]$username,
+    [string]$password
+)
+try {
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value $remote__Machine -Concatenate -Force
+    $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword
+    Invoke-Command -ComputerName $remote__Machine -Credential $credential -ScriptBlock {
+        Get-ChildItem -Path "C:\" -Directory | Select-Object -ExpandProperty Name  
+    }
+}
+catch {
+    Write-Error "An error occurred: $_"
+}
+finally {
+    Write-Output "Finally Block..."
+    # Disconnect-Vpn -ConnectionName $connectionName
+}
