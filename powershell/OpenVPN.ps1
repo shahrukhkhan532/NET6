f07fbf6867e34014814b5bf4afa12e40b6119ed3
shahrukhkhan532 <khanxhahrukh@gmail.com>
2023-04-12 23:23:33 +0500

    rrrrr

--- powershell/OpenVPN.ps1
+++ powershell/OpenVPN.ps1
@@ -8,14 +8,14 @@ function Import-Base64EncodedXML {
     Invoke-WebRequest -Uri "https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.9-I601-Win10.exe" -OutFile "openvpn-install-2.4.9-I601-Win10.exe"
     Start-Process -FilePath ".\openvpn-install-2.4.9-I601-Win10.exe" -ArgumentList "/S" -Wait
     $env:Path += ";C:\Program Files\OpenVPN\bin"
-
+    openvpn --version
     $decodedBytes = [System.Convert]::FromBase64String($EncodedFile)
     Set-Content -Path "output.ovpn" -Value $decodedBytes -Encoding Byte
     
     Rename-NetAdapter -InterfaceDescription "TAP-Windows Adapter V9" -NewName "Webnetworks"
     # Start-Job -ScriptBlock { openvpn --config "output.ovpn" }
     Start-Process -FilePath "openvpn" -ArgumentList "--config", '"output.ovpn"' -WindowStyle Hidden -RedirectStandardOutput "logs.txt"
-    Start-Sleep -Seconds 15
+    Start-Sleep -Seconds 10
     Get-Content "logs.txt"
     # openvpn --config "output.ovpn"
 }
--- powershell/sql.ps1
+++ powershell/sql.ps1
@@ -5,13 +5,7 @@
     [string]$Password
 )
 try {
-
-    # $instance = "10.4.0.4"
-    # $DbName = "iCM_STG"
-    # $UID = "TestDeployment"
-    # $Password = "TestDeployment$"
     $ConnectionString = 'Server=' + $instance + ';Database=' + $DbName + ';User Id=' + $UID + ';Password=' + $Password + ';Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;'
-
     Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT TOP 1 * FROM [User];" -ErrorAction 'Stop'
 
 }
@@ -20,5 +14,4 @@ catch {
 }
 finally {
     Write-Output "Finally Block..."
-    # Disconnect-Vpn -ConnectionName $connectionName
 }
