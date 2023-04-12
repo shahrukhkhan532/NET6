c4b23bdd229ee9b6cc1a8e57ddfe93282ce30c2b
shahrukhkhan532 <khanxhahrukh@gmail.com>
2023-04-12 23:27:00 +0500

    Update sql.ps1

--- powershell/sql.ps1
+++ powershell/sql.ps1
@@ -6,8 +6,8 @@
 )
 try {
     $ConnectionString = 'Server=' + $instance + ';Database=' + $DbName + ';User Id=' + $UID + ';Password=' + $Password + ';Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;'
+    Write-Output $ConnectionString
     Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT TOP 1 * FROM [User];" -ErrorAction 'Stop'
-
 }
 catch {
     Write-Error "An error occurred: $_"
