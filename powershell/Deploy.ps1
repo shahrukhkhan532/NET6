ef44201ca7c827236e71877613cf7865591e76ae
shahrukhkhan532 <khanxhahrukh@gmail.com>
2023-04-14 03:48:00 +0500

    swswsw

---
+++ powershell/Deploy.ps1
@@ -0,0 +1,31 @@
+$branch = "ftp_2"
+$sourcePath = Env.sourcePath
+$destinationPath = Env.destinationPath
+$appPoolName = Env.appPoolName
+
+Write-Output ($branch + " Branch Deployment. App Pool: " + $appPoolName + ", Source: " + $sourcePath + ", Destination: " + $destinationPath)
+
+$username = "webnetworks\Shahrukhsrk"
+$password = "skR00t@1234!!"
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword
+          
+Invoke-Command -ComputerName 10.4.0.5 -Credential $credential -ScriptBlock {
+    param(
+        $appPoolName
+    )
+    C:\Windows\system32\inetsrv\AppCmd.exe stop apppool /apppool.name:$appPoolName
+} -ArgumentList $appPoolName
+
+$sourcePath = (".\" + $branch + "_out")
+$destinationPath = "Z:\test\"
+$excludeFolders = "cs", "de", "es", "fr", "it", "ja", "ko", "pt-BR", "ru", "tr", "zh-Hans", "zh-Hant"
+robocopy $sourcePath $destinationPath /E /COPY:DAT /DCOPY:T /XO /NFL /NDL /R:5 /W:10 /MT:8 /XD $excludeFolders
+
+Invoke-Command -ComputerName 10.4.0.5 -Credential $credential -ScriptBlock {
+    param(
+        $appPoolName
+    )
+    C:\Windows\system32\inetsrv\AppCmd.exe start apppool /apppool.name:$appPoolName
+} -ArgumentList $appPoolName
+Set-Location ..
