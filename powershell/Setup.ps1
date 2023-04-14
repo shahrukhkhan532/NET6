02c5ccef2ee866e08ebde4b0ba2ab540acfdb923
shahrukhkhan532 <khanxhahrukh@gmail.com>
2023-04-14 22:22:26 +0500

    Update Setup.ps1

--- powershell/Setup.ps1
+++ powershell/Setup.ps1
@@ -45,28 +45,28 @@ function Get-Config {
 
     $config = @{
         'staging' = @{
-            'sourcePath'                 = (".\${branch}_out")
-            'appPoolName'                = 'API'
-            'CT__appPoolName'            = 'CareTrackerAPI'
-            'CT__sourcePath'             = ".\$branch_CTout"
-            'CT__destinationPath'        = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\CareTrackerAPI'
-            'destinationPath'            = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\API'
-            'Reports__destinationPath'   = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\APIReports\bin'
-            'path__SQL__To__Directory'   = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_SQL_Logs"
-            'path__Build__To__Directory' = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_Build_Logs"
-            'URL__Webhook'               = 'https://icaremanagerllc.webhook.office.com/webhookb2/d3a03a5f'
+            'sourcePath'               = (".\${branch}_out")
+            'appPoolName'              = 'API'
+            'CT__appPoolName'          = 'CareTrackerAPI'
+            'CT__sourcePath'           = ".\$branch_CTout"
+            'CT__destinationPath'      = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\CareTrackerAPI'
+            'destinationPath'          = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\API'
+            'Reports__destinationPath' = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory\APIReports\bin'
+            'SQL_LOG_File_Path'        = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_SQL_Logs\$((Get-Date -UFormat "%d-%m-%Y") + '.log')"
+            'BUILD_LOG_File_Path'      = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_Build_Logs\$((Get-Date -UFormat "%d-%m-%Y") + '.log')"
+            'URL__Webhook'             = 'https://icaremanagerllc.webhook.office.com/webhookb2/d3a03a5f'
         }
         'master'  = @{
-            'sourcePath'                 = (".\${branch}_out")
-            'appPoolName'                = 'API-Sandbox'
-            'CT__appPoolName'            = 'CT-Sandbox'
-            'CT__sourcePath'             = ".\$branch_CTout"
-            'CT__destinationPath'        = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\CareTrackerAPI'
-            'Reports__destinationPath'   = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\APIReports\bin'
-            'destinationPath'            = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\API'
-            'path__SQL__To__Directory'   = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_SQL_Logs"
-            'path__Build__To__Directory' = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_Build_Logs"
-            'URL__Webhook'               = 'https://icaremanagerllc.webhook.office.com/webhookb2/bf69bc38'
+            'sourcePath'               = (".\${branch}_out")
+            'appPoolName'              = 'API-Sandbox'
+            'CT__appPoolName'          = 'CT-Sandbox'
+            'CT__sourcePath'           = ".\$branch_CTout"
+            'CT__destinationPath'      = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\CareTrackerAPI'
+            'Reports__destinationPath' = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\APIReports\bin'
+            'destinationPath'          = Join-Path -Path $baseDestinationDir -ChildPath 'Working Directory - Sandbox\API'
+            'SQL_LOG_File_Path'        = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_SQL_Logs\$((Get-Date -UFormat "%d-%m-%Y") + '.log')"
+            'BUILD_LOG_File_Path'      = Join-Path -Path $baseDestinationDir -ChildPath "${branch}_Build_Logs\$((Get-Date -UFormat "%d-%m-%Y") + '.log')"
+            'URL__Webhook'             = 'https://icaremanagerllc.webhook.office.com/webhookb2/bf69bc38'
         }
     }
 
@@ -86,15 +86,6 @@ if ($null -eq $config) {
 foreach ($key in $config.Keys) {
     [System.Environment]::SetEnvironmentVariable($key, $config[$key], "User")
 }
-
-Add-DirectoryIfNotExists -path $config['path__SQL__To__Directory']
-Add-DirectoryIfNotExists -path $config['path__Build__To__Directory']
-
-$SQL_LOG_File_Path = Add-LogFileIfNotExists -path $config['path__SQL__To__Directory'] -fileName ((Get-Date -UFormat "%d-%m-%Y") + ".log")
-$BUILD_LOG_File_Path = Add-LogFileIfNotExists -path $config['path__Build__To__Directory'] -fileName ((Get-Date -UFormat "%d-%m-%Y") + ".log")
-
-[System.Environment]::SetEnvironmentVariable("SQL_LOG_File_Path", $SQL_LOG_File_Path, "User")
-[System.Environment]::SetEnvironmentVariable("BUILD_LOG_File_Path", $BUILD_LOG_File_Path, "User")
 [System.Environment]::SetEnvironmentVariable("branch", $branch, "User")
 
 foreach ($key in $config.Keys) {
